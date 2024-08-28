d4b03474a85e1a8238ad4045a0ae4c5d7752e451
Matthew Carby <66135007+MattKen18@users.noreply.github.com>
2024-08-28 00:38:00 -0400

    Create initial structure

---
+++ app/trials/[trialId]/logs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logs = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Logs
---
+++ app/trials/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TrialsPage = () => {
+  return (
+    <div>Trials Table</div>
+  )
+}
+
+export default TrialsPage
