6906dcae6fe0c59b56db286a66c15d7f00a622c0
Matthew Carby <66135007+MattKen18@users.noreply.github.com>
2024-08-28 22:56:47 -0400

    Make create log refining changes

--- typings.d.ts
+++ typings.d.ts
@@ -3,10 +3,11 @@ type LogDetails = {
   signature: string,
   typeOfVisit: "Remote" | "Onsite" | "Waiver Call",
   purposeOfVisit: "SIV" | "IMV" | "COV" | "Audit",
-  dateOfVisit: string,
+  dateOfVisit: string | Date,
 }
 
 type TrialDetails = {
+  id: string,
   investigatorName: string,
   protocol: string,
   siteVisit: string,
